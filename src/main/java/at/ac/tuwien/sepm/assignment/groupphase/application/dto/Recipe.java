a09ccf89fc43c50aabb9b80cbc6e9c4d12e29789
Bernhard Steindl <steindl.bernhard94@gmail.com>
2018-05-10 23:31:51 +0200

    #25425 Adjustment Recipe for RecipeIngredient

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/application/dto/Recipe.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/application/dto/Recipe.java
@@ -2,93 +2,96 @@ package at.ac.tuwien.sepm.assignment.groupphase.application.dto;
 
 import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Recipe {
-    private Integer id;
-    private String name;
-    private Double duration;
-    private String description;
-    private EnumSet<RecipeTag> tags;
-    private Boolean deleted;
+	private Integer id;
+	private String name;
+	private Double duration;
+	private String description;
+	private EnumSet<RecipeTag> tags;
+	private Boolean deleted;
+	private List<RecipeIngredient> recipeIngredients = null;
 
-    public Recipe(){
+	public Recipe() {
+		// NOOP
+	}
 
-    }
-
-    /**
-     * @param id
-     * @param name
-     * @param duration
-     * @param description
-     * @param tags
-     * @param deleted
-     */
-    public Recipe(Integer id, String name, Double duration, String description, EnumSet<RecipeTag> tags, Boolean deleted) {
-        this.id = id;
-        this.name = name;
-        this.duration = duration;
+	/**
+	 * @param id
+	 * @param name
+	 * @param duration
+	 * @param description
+	 * @param tags
+	 * @param deleted
+	 */
+	public Recipe(Integer id, String name, Double duration, String description, EnumSet<RecipeTag> tags,
+			Boolean deleted) {
+		this.id = id;
+		this.name = name;
+		this.duration = duration;
 		this.description = description;
-        this.tags = tags;
-        this.deleted = deleted;
-    }
-    
-    /**
-     * Constructor
-     * @param name
-     * @param duration
-     * @param description
-     * @param tags
-     */
-    public Recipe(String name, Double duration, String description, EnumSet<RecipeTag> tags) {
-        this.name = name;
-        this.duration = duration;
+		this.tags = tags;
+		this.deleted = deleted;
+	}
+
+	/**
+	 * Constructor
+	 * @param name
+	 * @param duration
+	 * @param description
+	 * @param tags
+	 */
+	public Recipe(String name, Double duration, String description, EnumSet<RecipeTag> tags) {
+		this.name = name;
+		this.duration = duration;
 		this.description = description;
-        this.tags = tags;
-    }
+		this.tags = tags;
+	}
 
-    /**
-     * @param id
-     * @param name
-     * @param duration
-     * @param description
-     * @param tagsAsString
-     * @param deleted
-     */
-    public Recipe(Integer id, String name, Double duration, String description, String tagsAsString, Boolean deleted) {
-        this.id = id;
-        this.name = name;
-        this.duration = duration;
+	/**
+	 * @param id
+	 * @param name
+	 * @param duration
+	 * @param description
+	 * @param tagsAsString
+	 * @param deleted
+	 */
+	public Recipe(Integer id, String name, Double duration, String description, String tagsAsString, Boolean deleted) {
+		this.id = id;
+		this.name = name;
+		this.duration = duration;
 		this.description = description;
-        setTagsAsString(tagsAsString);
-        this.deleted = deleted;
-    }
+		setTagsAsString(tagsAsString);
+		this.deleted = deleted;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public Double getDuration() {
-        return duration;
-    }
+	public Double getDuration() {
+		return duration;
+	}
 
-    public void setDuration(Double duration) {
-        this.duration = duration;
-    }
+	public void setDuration(Double duration) {
+		this.duration = duration;
+	}
 
-    public String getDescription() {
+	public String getDescription() {
 		return description;
 	}
 
@@ -97,44 +100,41 @@ public class Recipe {
 	}
 
 	public EnumSet<RecipeTag> getTags() {
-        return tags;
-    }
+		return tags;
+	}
 
-    public void setTags(EnumSet<RecipeTag> tags) {
-        this.tags = tags;
-    }
+	public void setTags(EnumSet<RecipeTag> tags) {
+		this.tags = tags;
+	}
 
-    public Boolean getDeleted() {
-        return deleted;
-    }
+	public Boolean getDeleted() {
+		return deleted;
+	}
 
-    public void setDeleted(Boolean deleted) {
-        this.deleted = deleted;
-    }
+	public void setDeleted(Boolean deleted) {
+		this.deleted = deleted;
+	}
 
-    public String getTagsAsString() {
-        return tags
-            .stream()
-            .map(RecipeTag::toString)
-            .collect(Collectors.joining());
-    }
+	public String getTagsAsString() {
+		return tags.stream().map(RecipeTag::toString).collect(Collectors.joining());
+	}
 
-    public void setTagsAsString(String tags) {
-        this.tags = Arrays
-            .stream(tags.split(""))
-            .map(RecipeTag::valueOf)
-            .collect(Collectors.toCollection(() -> EnumSet.noneOf(RecipeTag.class)));
-    }
+	public void setTagsAsString(String tags) {
+		this.tags = Arrays.stream(tags.split("")).map(RecipeTag::valueOf)
+				.collect(Collectors.toCollection(() -> EnumSet.noneOf(RecipeTag.class)));
+	}
 
-    @Override
-    public String toString() {
-        return "Recipe{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", duration=" + duration +
-            ", description='" + description + '\'' +
-            ", tags=" + tags +
-            ", deleted=" + deleted +
-            '}';
-    }
+	public List<RecipeIngredient> getRecipeIngredients() {
+		return recipeIngredients;
+	}
+
+	public void setRecipeIngredients(List<RecipeIngredient> recipeIngredients) {
+		this.recipeIngredients = recipeIngredients;
+	}
+
+	@Override
+	public String toString() {
+		return "Recipe{" + "id=" + id + ", name='" + name + '\'' + ", duration=" + duration + ", description='"
+				+ description + '\'' + ", tags=" + tags + ", deleted=" + deleted + '}';
+	}
 }
