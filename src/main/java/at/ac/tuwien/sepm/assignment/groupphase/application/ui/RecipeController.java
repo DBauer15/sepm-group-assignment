2876b26df4c07e4fef578d05eb723cef1af36851
david <david.bauer009@gmail.com>
2018-05-12 18:09:16 +0200

    #25425 Ingredient search and adding (UI)
    This commit adds search functionality for ingredients and adds the
    capability to add and remove them from the ingredient list.

--- src/main/java/at/ac/tuwien/sepm/assignment/groupphase/application/ui/RecipeController.java
+++ src/main/java/at/ac/tuwien/sepm/assignment/groupphase/application/ui/RecipeController.java
@@ -1,25 +1,45 @@
 package at.ac.tuwien.sepm.assignment.groupphase.application.ui;
 
+import at.ac.tuwien.sepm.assignment.groupphase.application.dto.IngredientSearchParam;
 import at.ac.tuwien.sepm.assignment.groupphase.application.dto.Recipe;
+import at.ac.tuwien.sepm.assignment.groupphase.application.dto.RecipeIngredient;
 import at.ac.tuwien.sepm.assignment.groupphase.application.service.RecipeService;
 import at.ac.tuwien.sepm.assignment.groupphase.application.service.ServiceInvokationException;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 
 import java.lang.invoke.MethodHandles;
+import java.net.URL;
+import java.util.*;
 
 import static at.ac.tuwien.sepm.assignment.groupphase.application.util.implementation.UserInterfaceUtility.showAlert;
 
 @Controller
-public class RecipeController {
+public class RecipeController implements Initializable {
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @FXML
     Label headerLabel;
+
+    @FXML
+    TextField ingredientAmountTextField;
+
+    @FXML
+    Label ingredientUnitLabel;
+
+    @FXML
+    GridPane ingredientsGridPane;
 
     @FXML
     TextField recipeNameTextField;
@@ -36,12 +56,17 @@ public class RecipeController {
     @FXML
     Label preparationTimeLabel;
 
+    @FXML
+    ComboBox<RecipeIngredient> ingredientComboBox;
+
     private RecipeService recipeService;
     private Recipe r;
     private boolean isInEditMode = false;
+    private List<RecipeIngredient> ingredients;
 
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
+        this.ingredients = new ArrayList<>();
     }
 
     void initializeView(Recipe r) {
@@ -59,9 +84,44 @@ public class RecipeController {
     }
 
     @FXML
-    public void initialize() {
+    public void initialize(URL location, ResourceBundle resources) {
         preparationTimeSlider.valueProperty().addListener((ChangeListener) -> preparationTimeLabel.textProperty().setValue(
         String.valueOf((int) preparationTimeSlider.getValue()) + " minutes"));
+
+        ingredientComboBox.setConverter(new StringConverter<RecipeIngredient>() {
+            @Override
+            public String toString(RecipeIngredient object) {
+                if (object == null) {
+                    return null;
+                }
+                return object.toString();
+            }
+
+            @Override
+            public RecipeIngredient fromString(String string) {
+                return ingredientComboBox.getSelectionModel().getSelectedItem();
+            }
+        });
+        ingredientComboBox.getSelectionModel().selectedItemProperty().addListener( (options, oldValue, newValue) -> {
+            if (newValue != null) {
+                ingredientUnitLabel.setText(newValue.getUnitName());
+            }
+        });
+    }
+
+    @FXML
+    public void onIngredientComboBoxKeyPressed(KeyEvent event) {
+        IngredientSearchParam searchParam = new IngredientSearchParam(ingredientComboBox.getEditor().getText());
+        updateIngredientSearch(searchParam);
+    }
+
+    @FXML
+    public void onAddButtonClicked(ActionEvent event) {
+        RecipeIngredient selection = ingredientComboBox.getSelectionModel().getSelectedItem();
+        if (selection != null && validateIngredientInputs()) {
+            selection.setAmount(Double.parseDouble(ingredientAmountTextField.getText()));
+            addIngredient(selection);
+        }
     }
 
     @FXML
@@ -71,6 +131,7 @@ public class RecipeController {
         r.setName(recipeNameTextField.getText());
         r.setDuration(preparationTimeSlider.getValue());
         r.setDescription(directionsTextArea.getText());
+        r.setRecipeIngredients(ingredients);
 
 
         try {
@@ -92,5 +153,82 @@ public class RecipeController {
     public void onCancelButtonClicked() {
         LOG.debug("Recipe dialog cancelled.");
         ((Stage) saveButton.getScene().getWindow()).close();
+    }
+
+    private void updateIngredientSearch(IngredientSearchParam searchParam) {
+        try {
+            List<RecipeIngredient> recipeIngredients = recipeService.searchIngredient(searchParam);
+            ingredientComboBox.setItems(FXCollections.observableArrayList(recipeIngredients));
+            ingredientComboBox.show();
+        } catch (ServiceInvokationException e) {
+            showAlert(Alert.AlertType.WARNING, "Error.", e.getContext().getErrors().toString());
+            LOG.error(e.getMessage());
+        }
+    }
+
+    private void addIngredient(RecipeIngredient recipeIngredient) {
+        final int row = ingredientsGridPane.getRowCount();
+
+        //Create UI elements to be added to the grid pane
+        Button removeButton = new Button("X");
+        removeButton.setOnAction( event -> {
+            deleteIngredient(recipeIngredient);
+        });
+
+        Label name = new Label(recipeIngredient.getIngredientName());
+        name.getStyleClass().add("text-white");
+        Label amount = new Label(String.valueOf(recipeIngredient.getAmount()));
+        amount.getStyleClass().add("text-white");
+        Label unit = new Label(recipeIngredient.getUnitName());
+        unit.getStyleClass().add("text-white");
+
+        // add a new row to the grid pane
+        ingredientsGridPane.addRow(row, name, amount, unit, removeButton);
+
+        // add ingredient to tracking list
+        ingredients.add(recipeIngredient);
+
+    }
+
+    private void deleteIngredient(RecipeIngredient ingredient) {
+        int row = ingredients.indexOf(ingredient);
+
+        Set<Node> deleteNodes = new HashSet<>();
+        for (Node child : ingredientsGridPane.getChildren()) {
+            // get index from child
+            Integer rowIndex = GridPane.getRowIndex(child);
+
+            // handle null values for index=0
+            int r = rowIndex == null ? 0 : rowIndex;
+
+            if (r > row) {
+                // decrement rows for rows after the deleted row
+                GridPane.setRowIndex(child, r-1);
+            } else if (r == row) {
+                // collect matching rows for deletion
+                deleteNodes.add(child);
+            }
+        }
+
+        // remove nodes from row
+        ingredientsGridPane.getChildren().removeAll(deleteNodes);
+
+        // remove from tracking list
+        ingredients.remove(ingredient);
+    }
+
+    private boolean validateIngredientInputs() {
+        if (ingredientAmountTextField.getText() == null) {
+            showAlert(Alert.AlertType.ERROR, "Information", "Ingredient amount needs to be set.");
+            return false;
+        }
+
+        try {
+            Double.parseDouble(ingredientAmountTextField.getText());
+        } catch (NumberFormatException e) {
+            showAlert(Alert.AlertType.ERROR, "Information", "Ingredient amount needs to be a decimal number.");
+            return false;
+        }
+        return true;
     }
 }
