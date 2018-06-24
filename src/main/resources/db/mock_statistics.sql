a366d25d30a95fe43b27702566d398eb63673cc9
david <david.bauer009@gmail.com>
2018-06-24 12:23:04 +0200

    #29123 Add mock statistics sql template

---
+++ src/main/resources/db/mock_statistics.sql
@@ -0,0 +1,227 @@
+/*
+RECIPE #1
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-20', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-19', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-18', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-17', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-16', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-15', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-14', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-13', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-12', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-11', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-10', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-09', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-08', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-07', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-06', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-05', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-04', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-03', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-02', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (1, 'B', 1, '2018-06-01', NOW());
+
+/*
+RECIPE #2
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-20', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-19', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-18', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-17', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-16', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-15', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-14', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-13', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-12', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-11', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-10', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-09', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-08', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-07', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-06', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (2, 'B', 1, '2018-05-05', NOW());
+
+/*
+RECIPE #3
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-20', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-19', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-18', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-17', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-16', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-15', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-14', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-13', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-12', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-11', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (3, 'B', 1, '2018-04-10', NOW());
+
+/*
+RECIPE #4
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-20', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-19', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-18', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-17', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-16', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-15', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-14', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-13', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (4, 'B', 1, '2018-03-12', NOW());
+
+/*
+RECIPE #5
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-20', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-19', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-18', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-17', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-16', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-15', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-14', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-13', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (5, 'B', 1, '2018-02-12', NOW());
+
+/*
+RECIPE #6
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-20', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-19', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-18', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-17', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-16', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (6, 'B', 1, '2018-01-15', NOW());
+
+/*
+RECIPE #7
+*/
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (7, 'B', 1, '2018-07-24', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (7, 'B', 1, '2018-07-23', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (7, 'B', 1, '2018-07-22', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (7, 'B', 1, '2018-07-21', NOW());
+INSERT INTO DIET_PLAN_SUGGESTION (recipe, tag, diet_plan_id, date, created_timestamp)
+VALUES (7, 'B', 1, '2018-07-20', NOW());
